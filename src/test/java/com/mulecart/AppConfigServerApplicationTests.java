5b26851023564b870ffe1c918cc9f69d64d73aaf
Rithuik Yerrabrolu <rithuik.yerrabrolu@sapiens.com>
2020-06-10 01:30:01 +0530

    Added Initial AppServer Coding to Repo

---
+++ src/test/java/com/mulecart/AppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
