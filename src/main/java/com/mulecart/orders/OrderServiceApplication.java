e67d42cb026467d8a01c7a06a0bb67b46558c743
rithuiketz <=rithuik.etz@gmail.com>
2020-06-17 22:21:04 +0530

    updated for zuul config

--- src/main/java/com/mulecart/orders/OrderServiceApplication.java
+++ src/main/java/com/mulecart/orders/OrderServiceApplication.java
@@ -3,6 +3,7 @@ package com.mulecart.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
