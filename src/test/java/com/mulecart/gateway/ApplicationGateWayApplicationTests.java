2525904bcbfc221b4a4675d81864dfa20c5d30b4
rithuiketz <=rithuik.etz@gmail.com>
2020-06-17 22:57:18 +0530

    zuul server

---
+++ src/test/java/com/mulecart/gateway/ApplicationGateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulecart.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationGateWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
