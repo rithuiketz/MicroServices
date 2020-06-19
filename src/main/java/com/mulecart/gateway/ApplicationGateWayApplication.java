b676cded45cf8d49d50eafa71c7385d0b3e2d69c
rithuiketz <=rithuik.etz@gmail.com>
2020-06-20 00:29:00 +0530

    uu

--- src/main/java/com/mulecart/gateway/ApplicationGateWayApplication.java
+++ src/main/java/com/mulecart/gateway/ApplicationGateWayApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import com.mulecart.gateway.filters.RequestFilter;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@ComponentScan(basePackageClasses=RequestFilter.class)
 public class ApplicationGateWayApplication {
 
 	public static void main(String[] args) {
